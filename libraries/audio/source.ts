afbca7fc3f0d96f2fc26e3355e571b33be4f5040
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2026-07-23 15:49:14 -0400

    fix to source

--- libraries/audio/source.ts
+++ libraries/audio/source.ts
@@ -1,42 +1,35 @@
 import { system } from "./system.ts";
 
 export const create = (type: OscillatorType, envelope = [[0, 0], [0, 1]]) => {
-  const [oscillator, ampKnob, panKnob] = [
-    system.createOscillator(),
-    system.createGain(),
-    system.createStereoPanner(),
-  ];
-
-  oscillator.type = type;
-  oscillator.connect(ampKnob).connect(panKnob).connect(system.destination);
-
-  const _play = (
-    frequency: number,
+  return (
+    frequencies = [],
     durationS: number,
-    delayS: number,
-    direction: number,
+    delayS = 0,
+    direction = 0,
   ) => {
-    const startTimeS = system.currentTime + delayS;
+    for (const frequency of frequencies) {
+      const [oscillator, ampKnob, panKnob] = [
+        system.createOscillator(),
+        system.createGain(),
+        system.createStereoPanner(),
+      ];
+      oscillator.type = type;
+      oscillator.connect(ampKnob).connect(panKnob).connect(system.destination);
 
-    oscillator.frequency.value = frequency;
+      const startTimeS = system.currentTime + delayS;
+      oscillator.frequency.setValueAtTime(frequency, startTimeS);
+      panKnob.pan.setValueAtTime(direction, startTimeS);
 
-    let elapsedTimeS = startTimeS;
-    ampKnob.gain.setValueAtTime(0, elapsedTimeS);
-    panKnob.pan.setValueAtTime(direction, elapsedTimeS);
-    for (const [timeBreakpointS, velocityBreakpoint] of envelope) {
-      elapsedTimeS += timeBreakpointS;
-      ampKnob.gain.linearRampToValueAtTime(
-        velocityBreakpoint,
-        elapsedTimeS,
-      );
+      let elapsedTimeS = startTimeS;
+      ampKnob.gain.setValueAtTime(0, elapsedTimeS);
+      for (const [timeBreakpointS, velocityBreakpoint] of envelope) {
+        elapsedTimeS += timeBreakpointS;
+        ampKnob.gain.linearRampToValueAtTime(velocityBreakpoint, elapsedTimeS);
+      }
+      ampKnob.gain.linearRampToValueAtTime(0, startTimeS + durationS);
+
+      oscillator.start(startTimeS);
+      oscillator.stop(startTimeS + durationS);
     }
-
-    oscillator.start(startTimeS);
-    oscillator.stop(startTimeS + durationS);
   };
-
-  return (frequencies = [], durationS: number, delayS = 0, direction = 0) =>
-    frequencies.forEach((frequency) =>
-      _play(frequency, durationS, delayS, direction)
-    );
 };
