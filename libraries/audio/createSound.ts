c6e55e30369e3ca416516e764e736c15dec03076
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2026-08-27 22:28:34 -0400

    oops, deleted this by accident

--- libraries/audio/createSound.ts
+++ libraries/audio/createSound.ts
@@ -55,7 +55,7 @@ export const createSound = (...definitions: SoundDefinition[]) => {
         knob.setValueAtTime(index === 0 ? 0 : state[index], startTime));
 
       let elapsedTime = startTime;
-      ampKnob.gain.(
+      ampKnob.gain.linearRampToValueAtTime(
         0,
         max(elapsedTime, startTime + duration),
       );
@@ -72,4 +72,3 @@ export const createSound = (...definitions: SoundDefinition[]) => {
       source.stop(startTime + duration);
     });
 };
-linearRampToValueAtTime
